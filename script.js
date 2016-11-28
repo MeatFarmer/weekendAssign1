44e5f92a5964e9956389841141e553b1e6da0699
Joseph Barsness <MeatFarmer@Josephs-MacBook-Pro.local>
2016-11-28 08:44:54 -0600

    updated commit

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-function myFunction() {
-document.getElementById("employeeSalary").push(employeeArray);
+console.log('start');
+
+var employeeArray= [];
+
+var monthlySalaryTotal = function () {
+  var salariesTotal = 0;
+  for (var i = 0; i < employeeArray.length; i++){
+  salariesTotal += Number(employeeArray[i][4]);
 }
-var employeeArray = [ ];
+salariesTotal = salariesTotal/12;
+console.log("Salary total:", salariesTotal);
+document.getElementById("monthlySal").innerHTML = salariesTotal;
+return (salariesTotal/12);
+};
+
+var myFunction = function() {
+// stores the information from the input and stores it into the array
+var firstName = document.getElementById('fname').value;
+var lastName = document.getElementById('lname').value;
+var idNum = document.getElementById('idnum').value;
+var jobTitle = document.getElementById('title').value;
+var annSal = document.getElementById('salary').value;
+// pushes to the array
+newEmployee = [firstName, lastName, idNum, jobTitle, annSal];
+console.log(newEmployee);
+employeeArray.push(newEmployee);
+// clearing the input fields
+document.getElementById('fname'). value = '';
+document.getElementById('lname').value = '';
+document.getElementById('idnum').value = '';
+document.getElementById('title').value = '';
+document.getElementById('salary').value = '';
+console.log(employeeArray);
+monthlySalaryTotal();
+document.getElementById('MST').innerHTML = employeeArray;
+};
